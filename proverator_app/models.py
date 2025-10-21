29db27a7273c6cc51cba57adf36ef442b05dbd7e
Krd2024 <161505798+Krd2024@users.noreply.github.com>
2025-10-21 22:01:48 +0300

    feat: добавить модели Domain, Request

--- proverator_app/models.py
+++ proverator_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Domain(models.Model):
+    domain = models.URLField(verbose_name="Домен", max_length=200)
+
+    class Meta:
+        verbose_name = "Домен"
+        verbose_name_plural = "Домены"
+
+
+    def __str__(self):
+        return self.domain
+
+class Request(models.Model):
+    domain = models.ForeignKey(Domain, verbose_name="Домен", on_delete=models.CASCADE)
+    status_code=models.IntegerField(verbose_name="Статус код")
+    response_time=models.DurationField(verbose_name="Время ответа")
+    verified_at  = models.DateTimeField(auto_now_add=True,verbose_name="Дата и время проверки")
+
+    class Meta:
+        verbose_name = "Запрос"
+        verbose_name_plural = "Запросы"
+
+    def __str__(self):
+        return f"{self.domain} - {self.status_code}"
