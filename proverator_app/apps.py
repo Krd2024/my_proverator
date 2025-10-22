40e390aae71dd81534944421e44fca6ca431f07b
Krd2024 <161505798+Krd2024@users.noreply.github.com>
2025-10-22 14:13:59 +0300

    chore: Cнять регистрацию моделей SolarSchedule и ClockedSchedule         из админки django_celery_beat

--- proverator_app/apps.py
+++ proverator_app/apps.py
@@ -4,3 +4,23 @@ from django.apps import AppConfig
 class ProveratorAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'proverator_app'
+
+    def ready(self):
+        """
+        Cнимает регистрацию моделей SolarSchedule и ClockedSchedule 
+        из админки django_celery_beat
+        """
+
+        from django.contrib import admin
+        from django_celery_beat.models import (
+
+            SolarSchedule,
+            ClockedSchedule,
+        )
+
+        try:
+            admin.site.unregister(SolarSchedule)
+            admin.site.unregister(ClockedSchedule)
+
+        except admin.sites.NotRegistered:
+            pass
